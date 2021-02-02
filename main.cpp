699c69c74507c7c63c88bea4b1d8f022b490c0a7
qmclouca <qmclouca@gmail.com>
2021-02-02 11:37:50 -0300

    Test: 3 6 6 eh perfeito 5 5 nao eh perfeito 28 28 eh perfeito
    Accepted by URI JUDGE.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int tests = 0, number = 0, sum =0;
+    std::cin >> tests;
+    for (int x=0; x<tests;x++){
+        std::cin >> number;
+        //std::cout << number << " inserido" << std::endl; //for test proposes
+        for (int x = 1; x<number; x++){
+            if (number%x == 0){
+                sum = sum + x;
+                //std::cout << sum << " soma parcial, valor somado: " << x << std::endl; //for test proposes
+            }
+        }
+        if (sum == number) {
+            std::cout << number << " eh perfeito" << std::endl;
+        } else {
+            std::cout << number << " nao eh perfeito" << std::endl;
+        }
+        sum = 0;
+    }
     return 0;
 }
